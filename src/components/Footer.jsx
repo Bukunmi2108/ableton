23595e3ce3d2c3ce07044f5dfd4047604e7d86e9
Bukunmi Akinyemi <bkakinyemi21@gmail.com>
2024-12-11 19:18:15 -0800

    first foreign commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,47 +11,77 @@ const Footer = () => {
       <section className='max-w-7xl mx-auto px-12 py-12'>
         <h3 className='font-roboto text-4xl font-bold mb-8'>Ableton</h3>
 
-        <div className='flex gap-12'>
-
-          <div className='flex flex-col gap-4'>
-            <div className='flex flex-col gap-1'>
-              <a className='font-roboto text-base tracking-wide' href="#">Register Live, Push or Move -</a>
-              <a className='font-roboto text-base tracking-wide' href="#">About Ableton -</a>
-              <a className='font-roboto text-base tracking-wide' href="#">Jobs -</a>
+        <div className='flex gap-12 flex-col'>
+          <div className='flex flex-col gap-12 lg:flex-row'>
+            <div className='flex flex-col gap-4'>
+              <div className='flex flex-col gap-1'>
+                <a className='font-roboto text-base tracking-wide' href="#">Register Live, Push or Move -</a>
+                <a className='font-roboto text-base tracking-wide' href="#">About Ableton -</a>
+                <a className='font-roboto text-base tracking-wide' href="#">Jobs -</a>
+              </div>
+              <div className='flex gap-2'>
+                <FaFacebook className='w-8 h-8 ' />
+                <FaSquareXTwitter className='w-8 h-8 ' />
+                <FaInstagramSquare className='w-8 h-8 ' />
+                <FaFacebook className='w-8 h-8 ' />
+                <FaSquareXTwitter className='w-8 h-8 ' />
+                <FaInstagramSquare className='w-8 h-8 ' />
+              </div>
             </div>
-            <div className='flex gap-2'>
-              <FaFacebook className='w-8 h-8 ' />
-              <FaSquareXTwitter className='w-8 h-8 ' />
-              <FaInstagramSquare className='w-8 h-8 ' />
-              <FaFacebook className='w-8 h-8 ' />
-              <FaSquareXTwitter className='w-8 h-8 ' />
-              <FaInstagramSquare className='w-8 h-8 ' />
+
+            <div className='flex flex-col gap-2'>
+              <h4 className='font-roboto text-base tracking-wide font-bold'>Education</h4>
+              <div className='flex flex-col gap-1'>
+                <a className='font-roboto text-base tracking-wide' href="#">Offers for students and teachers -</a>
+                <a className='font-roboto text-base tracking-wide' href="#">Ableton for the classroom -</a>
+                <a className='font-roboto text-base tracking-wide' href="#">Ableton for Colleges and Universities -</a>
+              </div>
+            </div>
+
+            <div className='flex flex-col gap-2'>
+              <h4 className='font-roboto text-base tracking-wide font-bold'>Sigin up to our newsletter</h4>
+              <div className='flex flex-col gap-1'>
+                <p className='font-roboto text-base tracking-wide leading-relaxed'>Enter your email address to stay up to date with the latest offers, tutorials, downloads, surveys and more.</p>
+              </div>
+              <form className='flex'>
+                <input 
+                  className='font-roboto outline-none p-2 bg-gray-200 w-full'
+                  placeholder='Email Address'
+                />
+                <button className='font-roboto py-2 px-4 bg-primary-6 text-white text-nowrap'>Sign up</button>
+              </form>
+            </div>
+          </div>
+          
+          <div className='flex gap-12'>
+
+            <div className='flex flex-col gap-2'>
+              <h4 className='font-roboto text-base tracking-wide font-bold'>Community</h4>
+              <div className='flex flex-col gap-1'>
+                <a className='font-roboto text-base tracking-wide' href="#">Find Ableton User Groups -</a>
+                <a className='font-roboto text-base tracking-wide' href="#">Find Certified Training -</a>
+                <a className='font-roboto text-base tracking-wide' href="#">Become Certified Trainer -</a>
+              </div>
             </div>
           </div>
 
-          <div className='flex flex-col gap-2'>
-            <h4 className='font-roboto text-base tracking-wide font-bold'>Education</h4>
-            <div className='flex flex-col gap-1'>
-              <a className='font-roboto text-base tracking-wide' href="#">Offers for students and teachers -</a>
-              <a className='font-roboto text-base tracking-wide' href="#">Ableton for the classroom -</a>
-              <a className='font-roboto text-base tracking-wide' href="#">Ableton for Colleges and Universities -</a>
-            </div>
-          </div>
+        </div>
 
-          <div className='flex flex-col gap-2'>
-            <h4 className='font-roboto text-base tracking-wide font-bold'>Sigin up to our newsletter</h4>
-            <div className='flex flex-col gap-1'>
-              <p className='font-roboto text-base tracking-wide leading-relaxed'>Enter your email address to stay up to date with the latest offers, tutorials, downloads, surveys and more.</p>
-            </div>
-            <form>
-              <input 
-                className='font-roboto outline-none p-2 bg-gray-200'
-                placeholder='Email Address'
-              />
-              <button className='font-roboto py-2 px-4 bg-primary-6 text-white'>Sign up</button>
-            </form>
-          </div>
+        <div className='flex items-start gap-12 lg:gap-0 lg:items-center lg:justify-between font-roboto text-sm tracking-wide mt-12 flex-col lg:flex-row'>
+          <ul className='flex flex-col lg:flex-row gap-2 lg:gap-6'>
+            <li><a href='#'>Contact</a></li>
+            <li><a href='#'>UsPress</a></li>
+            <li><a href='#'>Resources</a></li>
+            <li><a href='#'>Legal Info</a></li>
+            <li><a href='#'>Privacy Policy</a></li>
+            <li><a href='#'>Cookie Settings</a></li>
+            <li><a href='#'>Imprint</a></li>
+          </ul>
 
+          <div className='flex items-center justify-center gap-4'>
+            <img className='' src='logo.svg'/>
+            <p>Made in Berlin</p>
+          </div>
         </div>
       </section>
 
--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -5,8 +5,8 @@ const Jobs = () => {
   return (
     <div>
       <section className='w-full mx-auto px-12'>
-        <div className='mx-auto max-w-7xl bg-primary-3 flex flex-col'>
-          <img className='w-full object-cover' src={secImg6} />
+        <div className='mx-auto max-w-7xl bg-primary-3 flex flex-col lg:flex-row'>
+          <img className='w-full lg:w-1/2 object-cover' src={secImg6} />
           <div className='p-12 font-roboto flex flex-col gap-12'>
             <div className='text-left font-bold leading-7 lg:leading-10 tracking-wide text-xl lg:text-3xl'>
               We’re really proud of the work we’ve done so far. But there’s so much more to come. If you’d like to be a part of it, please join us.
